b6cee98f484c6ad8147b6e81d4abb5a6fa725ec2
Mailson <godoy.analytics@gmail.com>
2018-02-18 09:15:41 -0300

    update normalize script

--- scripts/download_normalize/normalize_files.sh
+++ scripts/download_normalize/normalize_files.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-# This script normalize the file NCM_SH, extrating from it 3 different files that will be used to load data into database. 
-# The extracted files are:
-# 1) NCM_SH4
-# 2) NCM_SH2
-# 3) NCM_SECROM
+
+#This script normalize the following files: 
+
+
+# 1) NCM_SH_UTF8
+# This file is normalized and generate 3 different files representing 3 tables in the database. The generated files are:
+#   a) NCM_SH4
+#   b) NCM_SH2
+#   c) NCM_SECROM
+#
+# 2) UF_MUN_UTF8
+#
+# 3) PORTO_UTF8
+#
+# 4) UF_UTF_8
+################################################################################################################################
 
 data_dir="../../data/raw_data"
 clean_data_dir="../../data/clean_data"
 
-# arquivo NCM_SH4
+# file NCM_SH4
 #cabecalho 
 sed -r -n '1s/","/"#"/pg' $data_dir/NCM_SH_UTF8.csv | cut -d# -f6-10 > $clean_data_dir/ncm_sh4
 #corpo 
 sed -r -n '2,$s/","/"#"/pg' $data_dir/NCM_SH_UTF8.csv | cut -d# -f6-10 | sort | uniq >> $clean_data_dir/ncm_sh4
 
-# arquivo NCM_SH2
+# file NCM_SH2
 #cabecalho 
 sed -r -n '1s/","/"#"/pg' $data_dir/NCM_SH_UTF8.csv | cut -d# -f10-14 > $clean_data_dir/ncm_sh2
 #corpo 
 sed -r -n '2,$s/","/"#"/pg' $data_dir/NCM_SH_UTF8.csv | cut -d# -f10-14 | sort | uniq >> $clean_data_dir/ncm_sh2
 
-# arquivo NCM_SECROM
+# file NCM_SECROM
 #cabecalho 
 sed -r -n '1s/","/"#"/pg' $data_dir/NCM_SH_UTF8.csv | cut -d# -f14-17 > $clean_data_dir/ncm_secrom
 #corpo 
